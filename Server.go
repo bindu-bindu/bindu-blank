b654b12f18e86cc91141ff72cdd8a973f25b051a
Shakhawat Hossain <whoshakhawat@gmail.com>
2019-09-12 05:42:21 +0600

    ping routes added

--- Server.go
+++ Server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/bindu-bindu/bindu-blank/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r = routes.API(r)
 	r.Run()
 }
--- routes/API.go
+++ routes/API.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// API REST ROUTES
+func API(r *gin.Engine) *gin.Engine {
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, "pong")
+	})
+
+	return r
+}
