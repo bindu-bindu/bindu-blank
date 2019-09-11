5c1f458374b8d9f1ac7c627941ba62e981c31d2a
Shakhawat Hossain <whoshakhawat@gmail.com>
2019-09-11 09:06:54 +0600

    Default properties fix

--- app/models/model.go
+++ app/models/model.go
@@ -2,8 +2,8 @@ package models
 
 import "time"
 
-// ModelUint64 Auto gen ID Primary, CreatedAt, UpdatedAt, DeletedAt Property
-type ModelUint64 struct {
+// DefaultProperties Auto gen ID Primary, CreatedAt, UpdatedAt, DeletedAt Property
+type DefaultProperties struct {
 	ID        uint64 `gorm:"primary_key" json:"-"`
 	CreatedAt time.Time
 	UpdatedAt time.Time  `json:"-"`
