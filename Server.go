8d6f76de503015566d4ade3018c952d2e57acd23
Shakhawat Hossain <whoshakhawat@gmail.com>
2019-09-14 13:54:20 +0600

    mysql connection added through .env

--- Server.go
+++ Server.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"github.com/bindu-bindu/bindu-blank/bindu"
+	"github.com/bindu-bindu/bindu-blank/db"
 	"github.com/bindu-bindu/bindu-blank/routes"
 	"github.com/gin-gonic/gin"
 )
 
+var err error
+
 func main() {
+	bindu.Init()
+	db.Con()
 	r := gin.Default()
 	r = routes.API(r)
+
+	defer db.DB.Close()
 	r.Run()
 }
--- routes/API.go
+++ routes/API.go
@@ -1,12 +1,27 @@
 package routes
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/bindu-bindu/bindu-blank/db"
 	"github.com/gin-gonic/gin"
 )
 
 // API REST ROUTES
 func API(r *gin.Engine) *gin.Engine {
 	r.GET("/ping", func(c *gin.Context) {
+
+		for {
+			pingErr := db.DB.DB().Ping()
+			if pingErr != nil {
+				fmt.Println(pingErr)
+			} else {
+				fmt.Println("Connected")
+			}
+			time.Sleep(time.Duration(3) * time.Second)
+		}
+
 		c.JSON(200, "pong")
 	})
 
