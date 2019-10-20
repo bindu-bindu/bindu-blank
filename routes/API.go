9d11df010b6eae3e4984ec0cf3a80c1f1faa7c93
Shakhawat Hossain <whoshakhawat@gmail.com>
2019-10-21 03:40:36 +0600

    Cors used in routes

--- routes/API.go
+++ routes/API.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/bindu-bindu/bindu-blank/app/controllers"
-
+	"github.com/bindu-bindu/bindu/World/app/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 // API REST ROUTES
 func API(r *gin.Engine) *gin.Engine {
+	r.Use(middlewares.Cors())
 	r.GET("/ping", controllers.Ping)
 
 	return r
